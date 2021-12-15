9560e70e708a490801db93390380178ad40d08aa
Reid Moffat <18rem8@queensu.ca>
2021-12-14 22:01:48 -0500

    fixed formatting and test settings

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,13 +2,9 @@
  * Connects and runs all of the tests with GoogleTest
  */
 
-#include <experimental/filesystem>
 #include <gtest/gtest.h>
 
 int main(int argc, char *argv[]) {
     testing::InitGoogleTest(&argc, argv);
-    const int testResult = RUN_ALL_TESTS();
-
-    std::experimental::filesystem::remove_all("../test");
-    return testResult;
+    return RUN_ALL_TESTS();
 }
