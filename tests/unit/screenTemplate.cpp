02747a768034e6f59590d29c7a3a1850d1c8f9a6
Reid Moffat <18rem8@queensu.ca>
2021-12-14 21:06:08 -0500

    tests cleanup

--- tests/unit/screenTemplate.cpp
+++ tests/unit/screenTemplate.cpp
@@ -2,53 +2,49 @@
  * Test cases for screenTemplate
  */
 
-//#include "../../include/screen/screenTemplate.h"
-//#include "../tests.h"
-//
-//using screen::ScreenTemplate;
-//
-//
-// * Dummy base class to tests abstract class 'functionality'
-// */
-//class DummyScreen : screen::ScreenTemplate {
-//public:
-//    int test;
-//
-//    DummyScreen() {}
-//
-//    void setT(int t) { test = t; }
-//
-//    DummyScreen(const DummyScreen &other) = default;
-//
-//    // Needed for dynamic memory
-//    ~DummyScreen() override = default;
-//
-//    // Must be overridden
-//    void run(sf::RenderWindow &gui) override {}
-//
-//    ScreenTemplate &operator=(const DummyScreen &rhs) {
-//        test = rhs.test;
-//        return *this;
-//    }
-//};
-//
-//
-//int main() {
-//    // These tests are mainly to understand base class implementation
-//    // and ensure the abstract class has a proper implementation
-//    DummyScreen testScreen;
-//    DummyScreen testScreen1;
-//
-//    testScreen.setT(10);
-//    testScreen1.setT(20);
-//
-//    ASSERT_OPERATION(testScreen.test, ==, 10);
-//    ASSERT_OPERATION(testScreen1.test, ==, 20);
-//    sf::RenderWindow rw;
-//    testScreen.run(rw);
-//    testScreen = testScreen1;
-//    ASSERT_OPERATION(testScreen.test, ==, 20);
-//    ASSERT_OPERATION(testScreen1.test, ==, 20);
-//
-//    return 0;
-//}
+#include "../../include/screen/screenTemplate.h"
+#include "../assertions.h"
+#include <gtest/gtest.h>
+
+using screen::ScreenTemplate;
+
+/**
+ * Dummy base class to tests abstract class 'functionality'
+ */
+class DummyScreen : screen::ScreenTemplate {
+private:
+    int test{};
+public:
+    DummyScreen() = default;
+
+    // Dummy methods
+    void setT(int t) { test = t; }
+    [[nodiscard]] int getT() const { return test; };
+    void run(sf::RenderWindow &gui) override {}
+
+    // Needed for dynamic memory
+    DummyScreen(const DummyScreen &other) = default;
+    ~DummyScreen() override = default;
+    DummyScreen &operator=(const DummyScreen &rhs) {
+        test = rhs.test;
+        return *this;
+    }
+};
+
+TEST(unit, screenTemplate) {
+    // These tests are mainly to understand base class implementation
+    // and ensure the abstract class has a proper implementation
+    DummyScreen testScreen;
+    DummyScreen testScreen1;
+
+    testScreen.setT(10);
+    testScreen1.setT(20);
+
+    ASSERT_OPERATION(testScreen.getT(), ==, 10);
+    ASSERT_OPERATION(testScreen1.getT(), ==, 20);
+    sf::RenderWindow rw;
+    testScreen.run(rw);
+    testScreen = testScreen1;
+    ASSERT_OPERATION(testScreen.getT(), ==, 20);
+    ASSERT_OPERATION(testScreen1.getT(), ==, 20);
+}
