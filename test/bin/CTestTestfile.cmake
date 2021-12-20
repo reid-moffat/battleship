e0705f436c69de8f9d4fdd2f8abef1cbe9fd122f
Reid Moffat <18rem8@queensu.ca>
2021-12-20 03:06:43 -0500

    added SFML to tests

---
+++ test/bin/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Reid Moffat/OneDrive - Queen's University/Coding/Projects/C++/battleship/test
+# Build directory: C:/Users/Reid Moffat/OneDrive - Queen's University/Coding/Projects/C++/battleship/test/bin
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("googletest")
---
+++ test/bin/googletest/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Reid Moffat/OneDrive - Queen's University/Coding/Projects/C++/battleship/test/googletest/googletest
+# Build directory: C:/Users/Reid Moffat/OneDrive - Queen's University/Coding/Projects/C++/battleship/test/bin/googletest/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
