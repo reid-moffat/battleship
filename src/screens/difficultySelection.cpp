f3636a4b9be6ab359535ce006963f5369f5c98cd
Reid Moffat <18rem8@queensu.ca>
2022-02-05 18:47:01 -0500

    removed redundant enum type names

--- src/screens/difficultySelection.cpp
+++ src/screens/difficultySelection.cpp
@@ -14,11 +14,11 @@ DifficultySelection::DifficultySelection() : ScreenTemplate() {
                                       "difficultySelection/IdleHardButton.png", "difficultySelection/ActiveHardButton.png",
                                       "difficultySelection/IdleBackButton.png", "difficultySelection/ActiveBackButton.png",
                                       "difficultySelection/IdleInstructionsButton.png", "difficultySelection/ActiveInstructionsButton.png"};
-    const vector<sprite> sprites = {{sf::Vector2f(0, 0), sf::Vector2f(5, 5), textureNames::Background_}};
-    const vector<button> buttons = {{sf::Vector2f(88 * 5, 92 * 5), sf::Vector2f(5, 5), textureNames::IdleEasyButton, textureNames::ActiveEasyButton},
-                                    {sf::Vector2f(200 * 5, 92 * 5), sf::Vector2f(5, 5), textureNames::IdleHardButton, textureNames::ActiveHardButton},
-                                    {sf::Vector2f(320 * 5, 12 * 5), sf::Vector2f(5, 5), textureNames::IdleBackButton, textureNames::ActiveBackButton},
-                                    {sf::Vector2f(352 * 5, 12 * 5), sf::Vector2f(5, 5), textureNames::IdleInstructionsButton, textureNames::ActiveInstructionsButton}};
+    const vector<sprite> sprites = {{sf::Vector2f(0, 0), sf::Vector2f(5, 5), Background_}};
+    const vector<button> buttons = {{sf::Vector2f(88 * 5, 92 * 5), sf::Vector2f(5, 5), IdleEasyButton, ActiveEasyButton},
+                                    {sf::Vector2f(200 * 5, 92 * 5), sf::Vector2f(5, 5), IdleHardButton, ActiveHardButton},
+                                    {sf::Vector2f(320 * 5, 12 * 5), sf::Vector2f(5, 5), IdleBackButton, ActiveBackButton},
+                                    {sf::Vector2f(352 * 5, 12 * 5), sf::Vector2f(5, 5), IdleInstructionsButton, ActiveInstructionsButton}};
 
     this->resources = ScreenResourceManager(texturePaths, sprites, buttons);
 }
@@ -33,10 +33,10 @@ DifficultySelection &screen::DifficultySelection::getInstance() {
 void DifficultySelection::update() {
     sf::Vector2f mousePosition = State::getMousePosition();
 
-    resources.getButton(buttonNames::EasyButton).updateButtonState(mousePosition);
-    resources.getButton(buttonNames::HardButton).updateButtonState(mousePosition);
-    resources.getButton(buttonNames::BackButton).updateButtonState(mousePosition);
-    resources.getButton(buttonNames::InstructionsButton).updateButtonState(mousePosition);
+    resources.getButton(EasyButton).updateButtonState(mousePosition);
+    resources.getButton(HardButton).updateButtonState(mousePosition);
+    resources.getButton(BackButton).updateButtonState(mousePosition);
+    resources.getButton(InstructionsButton).updateButtonState(mousePosition);
 }
 
 void DifficultySelection::poll() {
@@ -50,18 +50,18 @@ void DifficultySelection::poll() {
                 break;
             case sf::Event::MouseButtonReleased:
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    if (resources.getButton(buttonNames::EasyButton).getButtonState()) {
+                    if (resources.getButton(EasyButton).getButtonState()) {
                         State::difficulty = State::Difficulty::EASY;
                         State::changeScreen(Screens::FLEET_PLACEMENT);
                         break;
-                    } else if (resources.getButton(buttonNames::HardButton).getButtonState()) {
+                    } else if (resources.getButton(HardButton).getButtonState()) {
                         State::difficulty = State::Difficulty::HARD;
                         State::changeScreen(Screens::FLEET_PLACEMENT);
                         break;
-                    } else if (resources.getButton(buttonNames::BackButton).getButtonState()) {
+                    } else if (resources.getButton(BackButton).getButtonState()) {
                         State::changeScreen(Screens::GAME_MODE_SELECTION);
                         break;
-                    } else if (resources.getButton(buttonNames::InstructionsButton).getButtonState()) {
+                    } else if (resources.getButton(InstructionsButton).getButtonState()) {
                         State::changeScreen(Screens::INSTRUCTIONS);
                         break;
                     } else {
@@ -78,11 +78,11 @@ void DifficultySelection::render() {
     sf::RenderWindow &gui = *State::gui;
     gui.clear();
 
-    gui.draw(resources.getSprite(spriteNames::Background));
-    resources.getButton(buttonNames::EasyButton).render(gui);
-    resources.getButton(buttonNames::HardButton).render(gui);
-    resources.getButton(buttonNames::BackButton).render(gui);
-    resources.getButton(buttonNames::InstructionsButton).render(gui);
+    gui.draw(resources.getSprite(Background));
+    resources.getButton(EasyButton).render(gui);
+    resources.getButton(HardButton).render(gui);
+    resources.getButton(BackButton).render(gui);
+    resources.getButton(InstructionsButton).render(gui);
 
     if (State::getCurrentScreen() == Screens::DIFFICULTY_SELECTION) {
         gui.display();
