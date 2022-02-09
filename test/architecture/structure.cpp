84ef46721942a2a5ff1bd9495df614d34837360a
Reid Moffat <18rem8@queensu.ca>
2022-02-08 23:04:39 -0500

    Final clean-ups

--- test/architecture/structure.cpp
+++ test/architecture/structure.cpp
@@ -15,20 +15,19 @@ TEST(Architecture, requiredFiles) {
             "bin", "include", "res", "src", "test",
 
             // Build files
-            "CMakeLists.txt", "include/SFML",
+            "CMakeLists.txt", "include/SFML", "include/googletest",
 
             // Test files
-            "test/CMakeLists.txt", "test/googletest", "test/include/SFML", "test/main.cpp",
-            "test/assertions.hpp", "test/unit", "test/integration", "test/system",
-            "test/architecture",
+            "test/main.cpp", "test/assertions.hpp",
+            "test/architecture", "test/integration", "test/system", "test/unit",
 
             // VCS files
-            ".git", ".gitignore", "README.md", "LICENSE"};
-
+            ".git", ".gitignore", "README.md", "LICENSE",
+    };
 
     // Note: The current working directory of the test is the build/ folder; the same as the actual
     // application. This means we don't have to change the relative paths when referring to files
-    for (const string &file : files) {
+    for (const string &file: files) {
         if (access(("../" + file).c_str(), F_OK) == -1) {
             throw std::invalid_argument("File/directory not found in repository root: " + file);
         }
